645cb5476c465c6d16b971c18981563eff33ff29
twiggyweasel <orsusbass@gmail.com>
2015-08-26 18:10:58 +0000

    prepping gemfile for deployment

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,9 +2,6 @@ class Department < ActiveRecord::Base
    belongs_to :client 
    has_many :phones
    
-   scope :by_newest, -> {
-   order("name ASC")
-   }
-   
    default_scope { order('name ASC') } 
+
 end
